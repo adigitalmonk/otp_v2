f1ff7ef465a5535dac2b0b82694efffbd392b020
Jason T. Wong <jasontwong@users.noreply.github.com>
2021-06-28 15:32:23 -0400

    Add logging & Repool.Server multi

--- repool/lib/repool/application.ex
+++ repool/lib/repool/application.ex
@@ -7,14 +7,20 @@ defmodule Repool.Application do
 
   @impl true
   def start(_type, _args) do
+    IO.inspect("starting", label: :supervisor)
+
     children = [
       # Starts a worker by calling: Repool.Worker.start_link(arg)
-      {Repool.Server, "42"}
+      {Repool.Server, {"0", :sara}},
+      {Repool.Server, {"1", :brad}},
+      {Repool.Server, {"42", :bruce}},
+      {Repool.Server, {"2", :jason}},
+      {Repool.Server, {"24", :kaleb}}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Repool.Supervisor]
+    opts = [strategy: :rest_for_one, name: Repool.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
